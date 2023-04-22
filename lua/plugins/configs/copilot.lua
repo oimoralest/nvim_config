8feeb16dd9a0870cf5364d0a47db7b73bd047efd
oscarmorales <oscarmorales@habi.co>
2023-04-22 09:16:12 -0500

    Enable copilot globaly

--- lua/plugins/configs/copilot.lua
+++ lua/plugins/configs/copilot.lua
@@ -1,9 +1,3 @@
 vim.g.copilot_filetypes = {
-    ["*"] = false,
-    python = true,
-    javascript = true,
-    typescript = true,
-    yaml = true,
-    json = true,
-    lua = true,
+    ["*"] = true,
 }
