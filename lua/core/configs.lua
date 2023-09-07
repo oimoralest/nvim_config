bb75aa18a9026f0fa7021c605b958ef1b2f8fbb6
oscarmorales <oscarmorales@habi.co>
2023-09-07 16:59:12 -0500

    Enables clipboard compatibility with WSL

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -16,3 +16,16 @@ set.listchars:append({
 })
 set.termguicolors = true
 set.background = "dark"
+
+vim.g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
